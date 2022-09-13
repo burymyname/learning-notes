3cba6c650ca2511706cb89dfd175fce95eae4c6c
burymyname <burymyname@163.com>
2022-09-13 22:22:05 +0800

    update hw interview

--- notes/interviews/hw3.cc
+++ notes/interviews/hw3.cc
@@ -23,7 +23,33 @@ int main(int argc, char** argv) {
             return 0;
         }
 
-        // dp[i][j]: 从i开始
+        
+        vector<vector<int>> dp(N+1, vector<int>(1001, 0));
+        for (int j = 0; j <= 1000; ++j) {
+            int d = D - dis[N];
+            if (j >= d) dp[N][j] = d / 100;
+            else dp[N][j] = d/100 + wait[N] + 1;
+        }
+
+    
+        for (int i = N-1; i >= 0; --i) {
+            for (int j = 0; j <= 1000; ++j) {
+                int d = dis[i+1] - dis[i];
+                if (j >= d) {
+                    dp[i][j] = INT_MAX;
+                    if (dp[i+1][j-d] != -1) dp[i][j] = min(dp[i][j], dp[i+1][j-d] + d/100);
+                    if (dp[i+1][1000-d] != -1) dp[i][j] = min(dp[i][j], dp[i+1][1000-d] + d/100 + wait[i] + 1);
+                    if (dp[i][j] == INT_MAX) dp[i][j] = -1;
+                } else {
+                    if (dp[i+1][1000-d] == -1) dp[i][j] = -1;
+                    else {
+                        dp[i][j] = dp[i+1][1000-d] + d/100 + wait[i] + 1;
+                    } 
+                }
+            }
+        }
+
+        cout << dp[0][1000] << endl;        
     }
     return 0;
 }
